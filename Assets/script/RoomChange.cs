54738a977a6b816cc9513758cd095296dacea7d7
Miller-Anthony <zanderlof@gmail.com>
2023-02-04 13:33:55 -0800

    Room Change
    Added the ability to change scenes/room by hitting designated transfer areas

---
+++ Assets/script/RoomChange.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomChange : MonoBehaviour
+{
+    [SerializeField] string room;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene(room);
+    }
+}
