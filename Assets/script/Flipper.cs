1e4069df0673e4e2324116791036b5f35c563d06
Miller-Anthony <zanderlof@gmail.com>
2023-02-04 10:49:03 -0800

    Update Flipper.cs
    Updated Flipper script to work with individual flippers instead of being on a world object

--- Assets/script/Flipper.cs
+++ Assets/script/Flipper.cs
@@ -6,76 +6,81 @@ public class Flipper : MonoBehaviour
 {
     //variables to change
     [SerializeField] int flipperSpeed;
-
-    //game objects to manipulate the data of
-    [SerializeField] GameObject flipperL;
-    [SerializeField] GameObject flipperR;
-    [SerializeField] GameObject pointL;
-    [SerializeField] GameObject pointR;
+    [SerializeField] GameObject point;
 
     //data used for this script
-    private Vector3 flipperLStartPos;
-    private Quaternion flipperLStartRot;
-    private Vector3 flipperRStartPos;
-    private Quaternion flipperRStartRot;
+    private Vector3 flipperStartPos;
+    private Quaternion flipperStartRot;
     private bool moving;
     // Start is called before the first frame update
     void Start()
     {
-        flipperLStartPos = flipperL.transform.position;
-        flipperRStartPos = flipperR.transform.position;
-        flipperLStartRot = flipperL.transform.rotation;
-        flipperRStartRot = flipperR.transform.rotation;
+        flipperStartPos = transform.position;
+        flipperStartRot = transform.rotation;
         moving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //when you left click, the left flipper flips
-        if (Input.GetMouseButton(0))
+        switch(tag)
         {
-            if(flipperL.transform.rotation.y < flipperLStartRot.y + 90)
-            {
-                flipperL.transform.RotateAround(pointL.transform.position, Vector3.down, flipperSpeed * Time.deltaTime);
-                moving = true;
-            }
-            else
-            {
-                moving = false;
-            }
-        }
-        else if(flipperL.transform.rotation != flipperLStartRot)
-        {
-            flipperL.transform.RotateAround(pointL.transform.position, Vector3.down, -flipperSpeed * Time.deltaTime);
-            if(flipperL.transform.rotation.y > flipperLStartRot.y)
-            {
-                flipperL.transform.position = flipperLStartPos;
-                flipperL.transform.rotation = flipperLStartRot;
-            }
-        }
 
-        //when you right click, the right flipper flips
-        if (Input.GetMouseButton(1))
-        {
-            if (flipperR.transform.rotation.y < flipperRStartRot.y + 90)
-            {
-                flipperR.transform.RotateAround(pointR.transform.position, Vector3.up, flipperSpeed * Time.deltaTime);
-                moving = true;
-            }
-            else
-            {
-                moving = false;
-            }
+            case "Left":
+                //when you left click, the left flipper flips
+                if (Input.GetMouseButton(0))
+                {
+                    if (transform.rotation.y < flipperStartRot.y + 90)
+                    {
+                        transform.RotateAround(point.transform.position, Vector3.down, flipperSpeed * Time.deltaTime);
+                        moving = true;
+                    }
+                    else
+                    {
+                        moving = false;
+                    }
+                }
+                else if (transform.rotation != flipperStartRot)
+                {
+                    transform.RotateAround(point.transform.position, Vector3.down, -flipperSpeed * Time.deltaTime);
+                    if (transform.rotation.y > flipperStartRot.y)
+                    {
+                        transform.position = flipperStartPos;
+                        transform.rotation = flipperStartRot;
+                    }
+                }
+                break;
+            case "Right":
+                //when you right click, the right flipper flips
+                if (Input.GetMouseButton(1))
+                {
+                    if (transform.rotation.y < flipperStartRot.y + 90)
+                    {
+                        transform.RotateAround(point.transform.position, Vector3.up, flipperSpeed * Time.deltaTime);
+                        moving = true;
+                    }
+                    else
+                    {
+                        moving = false;
+                    }
+                }
+                else if (transform.rotation != flipperStartRot)
+                {
+                    transform.RotateAround(point.transform.position, Vector3.up, -flipperSpeed * Time.deltaTime);
+                    if (transform.rotation.y < flipperStartRot.y)
+                    {
+                        transform.position = flipperStartPos;
+                        transform.rotation = flipperStartRot;
+                    }
+                }
+                break;
+            default:
+                break;
         }
-        else if (flipperR.transform.rotation != flipperRStartRot)
-        {
-            flipperR.transform.RotateAround(pointR.transform.position, Vector3.up, -flipperSpeed * Time.deltaTime);
-            if (flipperR.transform.rotation.y < flipperRStartRot.y)
-            {
-                flipperR.transform.position = flipperRStartPos;
-                flipperR.transform.rotation = flipperRStartRot;
-            }
-        }
+    }
+
+    public bool IsMoving()
+    {
+        return moving;
     }
 }
