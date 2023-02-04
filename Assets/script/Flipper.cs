f40d5a3d8b8e80097dfaabd32af2d64ea199ddba
Spencer Rawls <rawlsspencer@gmail.com>
2023-02-04 14:34:49 -0800

    Fix flipper infinite rotation; improve flipper feel; make flipper prefabs

--- Assets/script/Flipper.cs
+++ Assets/script/Flipper.cs
@@ -5,82 +5,33 @@ using UnityEngine;
 public class Flipper : MonoBehaviour
 {
     //variables to change
-    [SerializeField] int flipperSpeed;
-    [SerializeField] GameObject point;
+    [SerializeField] string inputAxis;
+    [SerializeField] private float rotationTime;
+    [SerializeField] private float startRotation;
+    [SerializeField] private float endRotation;
 
-    //data used for this script
-    private Vector3 flipperStartPos;
-    private Quaternion flipperStartRot;
-    private bool moving;
-    // Start is called before the first frame update
-    void Start()
+    float targetRotation;
+    float currentRotation;
+    float currentRotationSpeed;
+
+	private void Awake()
+	{
+        targetRotation = startRotation;
+        currentRotation = startRotation;
+        currentRotationSpeed = 0f;
+        transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, currentRotation, 0);
+	}
+
+	void Update()
     {
-        flipperStartPos = transform.position;
-        flipperStartRot = transform.rotation;
-        moving = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        switch(tag)
-        {
-
-            case "Left":
-                //when you left click, the left flipper flips
-                if (Input.GetMouseButton(0))
-                {
-                    if (transform.rotation.y < flipperStartRot.y + 90)
-                    {
-                        transform.RotateAround(point.transform.position, Vector3.down, flipperSpeed * Time.deltaTime);
-                        moving = true;
-                    }
-                    else
-                    {
-                        moving = false;
-                    }
-                }
-                else if (transform.rotation != flipperStartRot)
-                {
-                    transform.RotateAround(point.transform.position, Vector3.down, -flipperSpeed * Time.deltaTime);
-                    if (transform.rotation.y > flipperStartRot.y)
-                    {
-                        transform.position = flipperStartPos;
-                        transform.rotation = flipperStartRot;
-                    }
-                }
-                break;
-            case "Right":
-                //when you right click, the right flipper flips
-                if (Input.GetMouseButton(1))
-                {
-                    if (transform.rotation.y < flipperStartRot.y + 90)
-                    {
-                        transform.RotateAround(point.transform.position, Vector3.up, flipperSpeed * Time.deltaTime);
-                        moving = true;
-                    }
-                    else
-                    {
-                        moving = false;
-                    }
-                }
-                else if (transform.rotation != flipperStartRot)
-                {
-                    transform.RotateAround(point.transform.position, Vector3.up, -flipperSpeed * Time.deltaTime);
-                    if (transform.rotation.y < flipperStartRot.y)
-                    {
-                        transform.position = flipperStartPos;
-                        transform.rotation = flipperStartRot;
-                    }
-                }
-                break;
-            default:
-                break;
-        }
+        bool activated = Input.GetButton(inputAxis);
+        targetRotation = activated ? endRotation : startRotation;
+        currentRotation = Mathf.SmoothDamp(currentRotation, targetRotation, ref currentRotationSpeed, rotationTime);
+        transform.localRotation = Quaternion.Euler(0, currentRotation, 0);
     }
 
     public bool IsMoving()
     {
-        return moving;
+        return !Mathf.Approximately(targetRotation, currentRotation);
     }
 }
